1ecd4a4c470845afaf66b41af37b30acdb045055
Noah Husain <noahhusain@gmail.com>
2022-06-06 11:48:41 -0500

    Fixing resume not showing

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,3 +4,18 @@ export function addResume(pdf) {
   const resumeButton = document.querySelector(".cta-btn--resume");
   resumeButton.setAttribute("href", pdf);
 }
+
+export function getGithub() {
+  return fetch("https://api.github.com/users/noahhusain").then((res) =>
+    res.json()
+  );
+}
+
+export function getHireable() {
+  const obj = getGithub();
+  const hireable = obj.hireable;
+  if (hireable === null) {
+    return "Hireable!";
+  }
+  return "Not Hireable";
+}
