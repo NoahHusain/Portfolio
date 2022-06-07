f86bb7008a1bfcac75201151b3d583d260589018
Noah Husain <noahhusain@gmail.com>
2022-06-07 09:52:28 -0500

    per instructor, github jobs is deprecated and no longer needed.

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,18 +4,3 @@ export function addResume(pdf) {
   const resumeButton = document.querySelector(".cta-btn--resume");
   resumeButton.setAttribute("href", pdf);
 }
-
-export function getGithub() {
-  return fetch("https://api.github.com/users/noahhusain").then((res) =>
-    res.json()
-  );
-}
-
-export function getHireable() {
-  const obj = getGithub();
-  const hireable = obj.hireable;
-  if (hireable === null) {
-    return "Hireable!";
-  }
-  return "Not Hireable";
-}
